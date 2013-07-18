caed71fc433d726e5ac76785a43d2b245a9a8bee
unknown <Administrator@rsaguier-PC.darriens.office>
2013-07-18 15:30:45 -0300

    ALGO DE VERDAD

---
+++ lib/functions.php
@@ -0,0 +1,9 @@
+<?php 
+function getCategorias() {}
+function getsubCategorias() {}
+function sendForm() {}
+function listarProductos() {}
+function getProductos($id=0){}
+function buscar($nombre){}
+
+?>
